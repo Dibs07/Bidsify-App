feb6b68d8afd5ee9ea1dc5c67a075817750f5a9e
Subhadeep-coder <dharsubhadeep5@gmail.com>
2024-06-29 01:01:37 +0530

    Fixed firebase

--- bidsify/lib/main.dart
+++ bidsify/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:notes/constants/constants.dart';
 import 'package:notes/screens/home_screen.dart';
@@ -5,8 +6,14 @@ import 'package:notes/screens/login_screen.dart';
 import 'package:notes/screens/registration_screen.dart';
 import 'package:notes/screens/onboarding_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -23,14 +30,14 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        textTheme: GoogleFonts.latoTextTheme(
-          Theme.of(context).textTheme,
-        ),
-        splashColor: Colors.transparent, // Set splash color to transparent
-        highlightColor: Colors.transparent, // Set highlight color to transparent
-        splashFactory: NoSplash.splashFactory, // Custom splash factory 
-        scaffoldBackgroundColor: kMobileBackgroundColor
-      ),
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          splashColor: Colors.transparent, // Set splash color to transparent
+          highlightColor:
+              Colors.transparent, // Set highlight color to transparent
+          splashFactory: NoSplash.splashFactory, // Custom splash factory
+          scaffoldBackgroundColor: kMobileBackgroundColor),
       // initialRoute: _user != null ? OnboardingScreen.id : HomeScreen.id,
       initialRoute: '/home_screen',
       routes: {
