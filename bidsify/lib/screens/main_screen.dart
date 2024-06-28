f3a20864301360ae9c4b9f8d817553fba58615cd
PratyushPal29 <29palpratyush04@gmail.com>
2024-06-29 01:09:19 +0530

    new file:   bidsify/assets/Ether-no-bg.png 	renamed:    bidsify/assets/Freelancer-bro (1).svg -> bidsify/assets/Freelancer-bro-1.svg 	modified:   bidsify/lib/screens/main_screen.dart

--- bidsify/lib/screens/main_screen.dart
+++ bidsify/lib/screens/main_screen.dart
@@ -13,11 +13,58 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kMobileBackgroundColor,
+      appBar: AppBar(
+        title: Text(
+              "Dibakar Banerjee",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+                fontSize: 20.0,
+              ),
+            ),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image(image: AssetImage('assets/logistics-blockchain-removebg-preview.png')),
           
+          Center(
+            child: Column(
+              children: [
+                Image(
+                  image: AssetImage('assets/Ether-no-bg.png'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40.0),
+                  child: Text("Wallet",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20.0,
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: Container(
+                    padding: EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      "Balance: ",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
