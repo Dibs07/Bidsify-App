a50d1e2e98615e88ef05dd7527855ed4b303f405
sayan <sayanadh123@gmail.com>
2024-06-29 11:20:22 +0530

    modified:   bidsify/lib/screens/main_screen.dart

--- bidsify/lib/screens/main_screen.dart
+++ bidsify/lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:notes/constants/constants.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,56 +14,66 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kMobileBackgroundColor,
-      appBar: AppBar(
-        title: Text(
-          "Dibakar Banerjee",
-          textAlign: TextAlign.left,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w900,
-            fontSize: 20.0,
-          ),
-        ),
-        backgroundColor: kMobileBackgroundColor,
-      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Center(
-            child: Column(
-              children: [
-                Image(
-                  image: AssetImage('assets/Ether-no-bg.png'),
+
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+                child: Text(
+                  'Welcome, username',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 40.0),
-                  child: Text("Wallet",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 20.0,
-                      )),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16.0),
-                  child: Container(
-                    padding: EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
+              ),
+            ),
+          ),
+
+          Expanded(
+            flex: 3,
+            child: Center(
+              child: Column(
+                children: [
+                  Image(
+                    image: AssetImage('assets/Ether-no-bg.png'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 40.0),
+                    child: Text("Wallet",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 20.0,
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Container(
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Text(
-                      "Balance: ",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
+                      child: Text(
+                        "Balance: ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
